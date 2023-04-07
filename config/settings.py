cb2cc72fee31d804ddcf5f7db715e69e5105adf5
Jamie Hamilton <jhamilton.contact@gmail.com>
2023-04-07 11:09:23 +0100

    :file_folder: Tried static files fix for production build

--- config/settings.py
+++ config/settings.py
@@ -160,3 +160,4 @@ else:
 # Heroku: Update database configuration from $DATABASE_URL.
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+django_heroku.settings(locals(),staticfiles=False)
